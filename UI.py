bda4672969d8fd09197244a5a80f4641feb48e2a
Phanisara <mild.mm38@gmail.com>
2022-06-12 23:35:30 +0700

    first commit

---
+++ UI.py
@@ -0,0 +1,40 @@
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt
+import sys
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # self.setGeometry(450, 200, 960, 540)
+        self.setStyleSheet("background-color: #DBDBDB;")
+        self.setFixedSize(1280, 800)
+        self.setWindowTitle("ระบบคัดแยกคุณภาพทุเรียนอัตโนมัติ")
+        self.setWindowIcon(QtGui.QIcon("pic/durian.png"))
+        self.show()
+    
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.white))
+        painter.setBrush(QBrush(Qt.white, Qt.SolidPattern))
+        # For title
+        painter.drawRoundedRect(67, 56, 776, 92, 20.0, 20.0)
+        # For status
+        painter.drawRoundedRect(860, 56, 351, 92, 20.0, 20.0)
+        # For picture
+        painter.drawRoundedRect(69, 181, 480, 550, 7.0, 7.0)
+        # For details
+        painter.drawRoundedRect(571, 181, 640, 550, 7.0, 7.0)
+        
+if __name__ == '__main__':
+    # create pyQt5 app
+    app = QApplication(sys.argv)
+
+    # create the instance of window
+    window = Window()
+    window.show()
+
+    # start the app
+    sys.exit(app.exec_())
