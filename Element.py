bf117f35f8db60e9a788c5d900c6ad5503cd9233
Phanisara <mild.mm38@gmail.com>
2022-06-13 14:20:03 +0700

    update text

---
+++ Element.py
@@ -0,0 +1,44 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class Color():
+    def __init__(self):
+      self.white      = "rgb(255, 255, 255)"
+      self.grey       = "rgb(172, 172, 172)" 
+      self.darkGrey   = "rgb(43, 43, 43)" 
+      self.yellow     = "rgb(255, 210, 85)" 
+      self.darkYellow = "rgb(97, 88, 0)"
+      self.lightGreen = "rgb(163, 195, 48)"
+      self.green      = "rgb(82, 137, 1)" 
+      self.darkGreen  = "rgb(56, 70, 9)" 
+      self.blackGreen = "rgb(28, 36, 0)" 
+      self.red        = "rgb(211, 47, 47)" 
+      self.darkRed    = "rgb(65, 0, 0)" 
+
+
+class Text():
+    def __init__(self, window, fontSize, word, posX, posY):
+        self.object = QLabel(window)
+        self.object.setText(word)
+        self.object.setFont(QFont("Prompt", fontSize))
+        self.object.move(posX, posY)
+        self.object.setAlignment(Qt.AlignCenter)
+
+    def setFontSize(self, fontSize):
+        self.object.setFont(QFont("Prompt", fontSize))
+      
+    def setPosition(self, posX, posY):
+        self.object.move(posX, posY)
+
+    def setSize(self, sizeX, sizeY):
+        self.object.resize(sizeX, sizeY)
+
+    def setStyle(self, style):
+        self.object.setStyleSheet(style)
+
+    def setText(self, text):
+        self.object.setText(text)
+
+    def normal(self):
+        self.object.setStyleSheet("color: rgb(255, 255, 255)")
--- UI.py
+++ UI.py
@@ -1,8 +1,11 @@
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPainter, QBrush, QPen, QFont, QFontDatabase
-from PyQt5.QtCore import Qt
 import sys
+from tokenize import Whitespace
+from webbrowser import BackgroundBrowser
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from Element import *
 
 class Window(QMainWindow):
     def __init__(self):
@@ -12,7 +15,62 @@ class Window(QMainWindow):
         self.setStyleSheet("background-color: #DBDBDB;")
         self.setFixedSize(1280, 800)
         self.setWindowTitle("ระบบคัดแยกคุณภาพผลทุเรียนอัตโนมัติ")
-        self.setWindowIcon(QtGui.QIcon("pic/durian.png"))
+        self.setWindowIcon(QtGui.QIcon("pics/durian.png"))
+
+        #---------------------------------------------------------------------------------------------------#
+
+        self.bigTopic = Text(self, 0, "ระบบคัดแยกคุณภาพผลทุเรียน", 125, 78)
+        self.bigTopic.setFontSize(21) 
+        self.bigTopic.setSize(460, 50)
+        self.bigTopic.setStyle("color: rgb(43, 43, 43); background-color: white; font-weight: bold;")
+
+        self.ByTopic = Text(self, 0, "BY BLUEBLINK @FIBO KMUTT", 600, 100)
+        self.ByTopic.setFontSize(7) 
+        self.ByTopic.setSize(185, 18)
+        self.ByTopic.setStyle("color: grey; background-color: white; font-weight: bold;") 
+
+        self.PicTopic = Text(self, 0, "PICTURE", 160, 210)
+        self.PicTopic.setFontSize(18) 
+        self.PicTopic.setSize(131, 46)
+        self.PicTopic.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: bold;") 
+
+        self.DetailTopic = Text(self, 0, "DETAILS", 670, 210)
+        self.DetailTopic.setFontSize(18) 
+        self.DetailTopic.setSize(131, 46)
+        self.DetailTopic.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: bold;") 
+
+        self.WeightTopic = Text(self, 0, "น้ำหนักผลทุเรียน", 610, 300)
+        self.WeightTopic.setFontSize(13) 
+        self.WeightTopic.setSize(150, 30)
+        self.WeightTopic.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: light;") 
+        
+        self.AmountTopic = Text(self, 0, "จำนวนพู", 795, 300)
+        self.AmountTopic.setFontSize(13) 
+        self.AmountTopic.setSize(150, 30)
+        self.AmountTopic.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: light;") 
+
+        self.PercentTopic = Text(self, 0, "เปอร์เซ็นต์น้ำหนักแห้ง", 970, 300)
+        self.PercentTopic.setFontSize(13) 
+        self.PercentTopic.setSize(210, 30)
+        self.PercentTopic.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: light;") 
+
+        self.WeightTopic1 = Text(self, 0, "กิโลกรัม", 610, 500)
+        self.WeightTopic1.setFontSize(13) 
+        self.WeightTopic1.setSize(150, 30)
+        self.WeightTopic1.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: light;") 
+
+        self.AmountTopic1 = Text(self, 0, "พู", 790, 500)
+        self.AmountTopic1.setFontSize(13) 
+        self.AmountTopic1.setSize(150, 30)
+        self.AmountTopic1.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: light;") 
+        
+        self.PercentTopic1 = Text(self, 0, "เปอร์เซ็นต์", 990, 500)
+        self.PercentTopic1.setFontSize(13) 
+        self.PercentTopic1.setSize(150, 30)
+        self.PercentTopic1.setStyle("color: rgb(56, 70, 9); background-color: white; font-weight: light;") 
+
+        #---------------------------------------------------------------------------------------------------#
+        
         self.show()
         
     
@@ -29,20 +87,8 @@ class Window(QMainWindow):
         # For details
         painter.drawRoundedRect(571, 181, 640, 550, 7.0, 7.0)
 
-    def addText(self):
-        font = QFont()
-        font.setFamily('Tahoma')
-        mainText = QLabel('ระบบคัดแยกผลทุเรียนอัตโนมัติ',window)
-        mainText.setFont(font)
-        mainText.move(129, 83)
-        
+
 if __name__ == '__main__':
-    # create pyQt5 app
     app = QApplication(sys.argv)
-
-    # create the instance of window
-    window = Window()
-    window.show()
-
-    # start the app
+    DurianUI = Window()
     sys.exit(app.exec_())
